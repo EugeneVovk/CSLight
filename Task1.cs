26c5cdb7b1b824e63978910c0e1a0d9c3e6ef296
Eugene Vovk <eugenevovk@gmail.com>
2022-12-31 01:12:48 +0300

    add Task1.cs

---
+++ Task1.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace CSLight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isDept = true;
+            ulong americanDebt = 31_466_983_123_456;
+            sbyte outdoorTemperature = -10;
+            int speed = 300_000;
+            string name = "Tom";
+            byte age = 30;
+            char grade = 'A';
+            ushort salaryAfter = 65000;
+            short salaryBefore = 30000;
+            float itemPrice = 400.05f;
+        }
+    }
+}
