de709b4e9e4e63cdcfc29fbdb65e79f4b57c3657
Eugene Vovk <eugenevovk@gmail.com>
2023-01-04 18:48:13 +0300

    Examples_from_the_lesson 'Shop'

---
+++ Shop1/Shop1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+/**
+ * Магазин, в котором будем продавать еду
+ * Дано: деньги, еда, еда за единицу еды
+ * Магазин не может давать вдолг =>
+ * Без лишних проверок получить результат невозможности ухода в отрицательный баланс
+ */
+namespace Shop1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int money;
+            int food;
+            int foodUnitPrice = 10;
+            bool isAbleToPay;
+
+            Console.WriteLine($"Добро пожаловать в пекарню!"
+                + $" Сегодня еда по {foodUnitPrice} монет");
+            Console.WriteLine($"Сколько у тебя золота?");
+            money = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Сколько тебе нужно еды?");
+            food = Convert.ToInt32(Console.ReadLine());
+
+            isAbleToPay = money >= food * foodUnitPrice;
+            food *= Convert.ToInt32(isAbleToPay);
+            money -= food * foodUnitPrice;
+            Console.WriteLine($"У тебя в сумке {food} единиц еды и {money} монет");
+        }
+    }
+}
