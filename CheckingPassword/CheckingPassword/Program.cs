76b0933a29ccfd50521abca8a89f7d18fe1700c4
Eugene Vovk <eugenevovk@gmail.com>
2023-01-04 18:51:57 +0300

    Examples_from_the_lesson 'CheckingPassword'

---
+++ CheckingPassword/CheckingPassword/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckingPassword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "123qwe";
+            string userInput;
+
+            Console.Write("Введите пароль: ");
+            userInput = Console.ReadLine();
+
+            if (userInput == password)
+            {
+                Console.WriteLine("Пароль принят.\nДоступ к базе данных разрешён.\n");
+            }
+            else
+            {
+                Console.WriteLine("Неверный пароль.\nДоступ закрыт.\n");
+            }
+        }
+    }
+}
