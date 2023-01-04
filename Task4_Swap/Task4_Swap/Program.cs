e91f113b8c1b784180d4a1f4299dc0fa9870df0f
Eugene Vovk <eugenevovk@gmail.com>
2023-01-04 20:06:08 +0300

    add program 'Swap'

---
+++ Task4_Swap/Task4_Swap/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+/**
+ * Даны две переменные. 
+ * Поменять местами значения двух переменных. 
+ * Вывести на экран значения переменных до перестановки и после.
+ * К примеру, есть две переменные имя и фамилия, они сразу инициализированные, 
+ * но данные не верные, перепутанные. 
+ * Вот эти данные и надо поменять местами через код.
+ */
+namespace Task4_Swap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "John";
+            string surname = "Paul";
+           
+            string temp = name;
+            name = surname;
+            surname = temp;
+
+            Console.WriteLine($"name = {name}, surname = {surname}");
+        }
+    }
+}
