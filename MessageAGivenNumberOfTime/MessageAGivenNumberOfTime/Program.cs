2e0780532a8579879ad19ad6d24ec0f3221e25ce
Eugene Vovk <eugenevovk@gmail.com>
2023-01-08 20:00:50 +0300

    add MessageAGivenNumberOfTime

---
+++ MessageAGivenNumberOfTime/MessageAGivenNumberOfTime/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MessageAGivenNumberOfTime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userReply;
+            int numberOfLoop;
+
+            Console.WriteLine("Что ты хочешь сказать?");
+            userReply= Console.ReadLine();
+            Console.WriteLine("Сколько раз ты готов это повторить?");
+            numberOfLoop= Convert.ToInt32(Console.ReadLine()); 
+
+            for (int i = 0; i < numberOfLoop; i++)
+            {
+                Console.WriteLine(userReply);
+            }
+        }
+    }
+}
