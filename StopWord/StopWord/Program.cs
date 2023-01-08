89bacfdf1a8dd216bae4363852fcdd7fc8117f0a
Eugene Vovk <eugenevovk@gmail.com>
2023-01-08 20:43:44 +0300

    add program 'StopWord'

---
+++ StopWord/StopWord/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StopWord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string stopWord = "exit";
+            string userStopWord = null;
+
+            while (stopWord != userStopWord)
+            {
+                Console.Write("Скажите особое слово: ");
+                userStopWord = Console.ReadLine();
+            }
+
+            Console.WriteLine($"\nВы сказали '{userStopWord}'\nВаше право :)");
+        }
+    }
+}
