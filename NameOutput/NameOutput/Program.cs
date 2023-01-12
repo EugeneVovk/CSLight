8d6649d709b49b70b4a46c93f5c5448f8a65bbde
Eugene Vovk <eugenevovk@gmail.com>
2023-01-12 17:15:02 +0300

    add 'NameOutput' - display the name in a frame

---
+++ NameOutput/NameOutput/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace NameOutput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName = "";
+            char userSymbol;
+
+            Console.WriteLine("Привет!");
+            Console.Write("Как тебя зовут? ");
+            userName = Console.ReadLine();
+            Console.Write("Введи любой символ ");
+            userSymbol = Convert.ToChar(Console.ReadLine());
+
+            int sizeName = userName.Length;
+            int row = sizeName + 2;
+            int column = 3;
+            int count = 0;
+
+            for (int i = 0; i < column; i++)
+            {
+                for (int j = 0; j < row; j++)
+                {
+                    if (i == 1 && j > 0 && j < row - 1)
+                    {
+                        Console.Write(userName[count++]);
+                    }
+                    else
+                    {
+                        Console.Write(userSymbol);
+                    }
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
