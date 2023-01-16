0ed40c500c41304d40bddbc37e7eac096b7281ab
Eugene Vovk <eugenevovk@gmail.com>
2023-01-16 17:26:39 +0300

    add 'Split'

---
+++ Split/Split/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Split
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string titleString = "Научим делать игры на Unity";
+            string[] strings = titleString.Split(' ');
+
+            foreach (string word in strings)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
