ed76144090eb08482926ad03e55620e5ae188047
Eugene Vovk <eugenevovk@gmail.com>
2023-01-17 23:33:41 +0300

    add method 'Suffle'

---
+++ Shuffle/Shuffle/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Shuffle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+
+            PrintArray(array);
+
+            ShuffleArray(array);
+
+            PrintArray(array);
+        }
+
+        private static void ShuffleArray(int[] array)
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Swap(array, i, i + random.Next(array.Length - i));
+            }
+        }
+
+        private static void Swap(int[] array, int a, int b)
+        {
+            int temp = array[a];
+            array[a] = array[b];
+            array[b] = temp;
+        }
+
+        private static void PrintArray(int[] array)
+        {
+            foreach (int number in array)
+            {
+                Console.Write(number + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
