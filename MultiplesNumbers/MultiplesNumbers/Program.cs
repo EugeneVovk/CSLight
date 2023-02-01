a4869525b77d163ff07f9dbc83c3a83287c1b327
Eugene Vovk <eugenevovk@gmail.com>
2023-02-02 01:05:54 +0300

    add app 'MultiplesNumbers'

---
+++ MultiplesNumbers/MultiplesNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace MultiplesNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int userNumber;
+            int lowerLimit = 100;
+            int upperLimit = 999;
+            int countMultiples = 0;
+            int threeSecondWaite = 3000;
+
+            Console.SetCursorPosition(5, 5);
+            Console.WriteLine("Давай поиграем!\n");
+            Console.Write(" Введи любое число от 1 до 27 включительно: ");
+            userNumber= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"\n ...а я постараюсь вывести количество трехзначных натуральных чисел, которые кратны {userNumber}.");
+
+            for(int i = userNumber; i < 1000; i+= userNumber) {
+                if (i >= lowerLimit && i <= upperLimit )
+                {
+                    countMultiples++;
+                }
+            }
+
+            Thread.Sleep(threeSecondWaite);
+
+            Console.WriteLine("\n\tТа-дам: " + countMultiples + "\n");
+        }
+    }
+}
