132211c51885cdbb5096a9478c1cefef677ac8ba
Eugene Vovk <eugenevovk@gmail.com>
2023-02-01 18:48:48 +0300

    add 'Picture'

---
+++ Pictures/Pictures/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Pictures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pictureInRow = 3;
+            int userAlbum = 52;
+            int fullFillRow;
+            int offRowPicture;
+
+            fullFillRow = userAlbum / pictureInRow;
+            offRowPicture = userAlbum % pictureInRow;
+
+            Console.WriteLine($" Количество полностью заполненных рядов: {fullFillRow}");
+            Console.WriteLine($" Картинок будет сверх меры: {offRowPicture}");
+        }
+    }
+}
