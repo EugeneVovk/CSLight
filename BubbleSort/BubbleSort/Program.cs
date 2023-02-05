672d22a7e892676693d4d790cf2b0d00ce2e7e5a
Eugene Vovk <eugenevovk@gmail.com>
2023-02-05 03:43:38 +0300

    add 'BubbleSort'

---
+++ BubbleSort/BubbleSort/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace BubbleSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[10];
+
+            Random random = new Random();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(-99, 100);
+            }
+
+            PrintArray(numbers);
+
+            BubbleSort(numbers);
+
+            PrintArray(numbers);
+        }
+
+        private static void BubbleSort(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - 1 - i; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        (array[j], array[j + 1]) = (array[j + 1], array[j]);
+                    }
+                }
+            }
+        }
+
+        private static void PrintArray(int[] array)
+        {
+            foreach (int item in array)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
