8a5de66211e79a9d2c10f2ccc842452dfe6a5305
Eugene Vovk <eugenevovk@gmail.com>
2023-02-14 07:22:20 +0300

    add 'ContainsDuplicate'

---
+++ LeetCodeContainsDuplicate/LeetCodeContainsDuplicate/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+/**
+* Дан массив чисел.
+* Вернуть true, если какое-либо значение встречается в массиве минимум дважды, т.е. содержит дубликат
+* Или вернуть false, если каждый элемент различен
+*/
+
+namespace LeetCodeContainsDuplicate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 3, 3 };
+
+            Console.WriteLine(ContainsDuplicate(nums));
+        }
+
+        public static bool ContainsDuplicate(int[] nums)
+        {
+            HashSet<int> set = new HashSet<int>(nums);
+
+            return nums.Length != set.Count;
+        }
+    }
+}
